4bd6fbd30c798906a95909f85162b0b5c16bb793
Alexander Hamme <ah2166@bard.edu>
2017-09-26 12:28:49 -0400

    Update animal.py

--- animal.py
+++ animal.py
@@ -5,7 +5,6 @@ import math
 class Animal:
 
     DEFAULT_BUFFER = 24  # length of line trailing behind each animal
-    VELOCITY_MULTIPLIER = 1000.0
 
     def __init__(self, point, r, t):
         self.x, self.y = point
@@ -19,19 +18,18 @@ class Animal:
         self.selection_index = 5                                    # buffer of previous data points to run calculations on
 
     def update_location(self, point, r, t):
-        # calculate speed, accel, etc.
-        # finally, update coordinates
+        # update kinematics values and append new data points
         self.x, self.y = point
 
-        self.parse_physics(point, t)
+        self.calc_kinematics(point, t)
 
         self.data_points.append((t, (self.x, self.y), self.velocity_vector, self.accel_vector, self.current_direction))
 
         self.line_points.appendleft((int(point[0]), int(point[1])))
 
-    def parse_physics(self, new_pt, t):
+    def calc_kinematics(self, new_pt, t):
 
-        if len(self.data_points) < self.selection_index:            # don't run calculations until there are enough data points
+        if len(self.data_points) < self.selection_index:            # don't run calculations until list has enough data points
             return
 
         prev_points = self.data_points[-self.selection_index:]
@@ -39,20 +37,21 @@ class Animal:
         x_points = [pt[1][0] for pt in list(prev_points)] + [new_pt[0]]  # make x and y lists of past coordinates + current coordinate
         y_points = [pt[1][1] for pt in list(prev_points)] + [new_pt[1]]
 
-        delta_time = t - prev_points[0][0]      # time elapsed between current point and the first in list
+        delta_time = (t - prev_points[0][0])/1000.0     # time elapsed between current point and the first in list. 
+        # convert from milliseconds to seconds so kinematics values are higher than zero
 
         dx = x_points[-1] - x_points[0]
         dy = y_points[-1] - y_points[0]
 
-        vx = self.VELOCITY_MULTIPLIER * dx / delta_time
-        vy = -self.VELOCITY_MULTIPLIER * dy / delta_time            # y axis must be reversed
+        vx = dx / delta_time
+        vy = -1 * dy / delta_time            # y axis must be reversed
 
         angle = 180/math.pi * math.atan2(vy, vx)
 
         # Use navigational bearings - straight up is 0 degrees.
         self.current_direction = 90 - angle if angle >= -90 else -(270 + angle)
         
-        # Other calculations - need revision
+        # Other kinematics calculations - this section is currently commented out because it is under revision
         '''
         if len(self.data_points) > self.selection_index:
 
@@ -67,14 +66,12 @@ class Animal:
 
             accel_x0 = self.data_points[-1][4][0]
             accel_y0 = self.data_points[-1][4][1]
-
-            # We only care about the scalar values of velocity? NO, shouldn't be.... ?
-
-            # Alternatives:
+            
+            # alternative equations:
 
             # accel_x = (float(vx)**2 - float(vx_0)**2) / (2*float(x_displacement))
             # accel_y = (float(vy)**2 - float(vy_0)**2) / (2*float(y_displacement))
-            # accel_x = (vx - vx_0) / time_elapsed  # (abs(vx) - abs(vx_0)) # Convert time to seconds for bigger decimals
+            # accel_x = (vx - vx_0) / time_elapsed  # (abs(vx) - abs(vx_0))
             # accel_y = (vy - vy_0) / time_elapsed       # abs(vy) - abs(vy_0)
 
             # accel_x = 0 if abs(accel_x) < self.ACCEL_MIN else accel_x
@@ -130,7 +127,7 @@ class Animal:
 
             # accel_x = 1000.0 * (float(vx)**2 - float(vx_0)**2) / (2*float(x_displacement))
             # accel_y = 1000.0 * (float(vy)**2 - float(vy_0)**2) / (2*float(y_displacement))
-            # or:
+            ### alternative kinematics equations:
             # accel_x = 1000.0 * (x_displacement - (vx_0 * time_elapsed))/(0.5*(time_elapsed**2))
             # accel_y = 1000.0 * (y_displacement - (vy_0 * time_elapsed))/(0.5*(time_elapsed**2))
             '''
